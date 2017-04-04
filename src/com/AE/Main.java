d38a1f31de591e9e36d8934a87dd84275f9c734a
Angel Rodriguez <arrmixer@gmail.com>
2017-04-04 11:47:08 -0400

    Initial commit

---
+++ src/com/AE/Main.java
@@ -0,0 +1,17 @@
+package com.AE;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.print("input a number to find it's time table: ");
+        X x = new X();
+        x.scanner.nextLine();
+        x.x();
+
+
+
+
+    }
+}
---
+++ src/com/AE/X.java
@@ -0,0 +1,21 @@
+package com.AE;
+
+import java.util.Scanner;
+
+/**
+ * Created by Angel on 1/11/17.
+ */
+public class X {
+    Scanner scanner = new Scanner(System.in);
+    private int x = scanner.nextInt();
+
+
+    public void x(){
+
+        for (int i = 0; i<= 12;i++){
+            System.out.println(x + " times " + i + " = " + x*i);
+        }
+
+    }
+
+}
